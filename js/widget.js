60c248cb71deae5a14e39787f6e68c33be1913c8
Gabriel Furet <gfuret@gmail.com>
2014-07-04 14:06:45 +0300

    add rooms to status

--- js/widget.js
+++ js/widget.js
@@ -2,17 +2,33 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function(){
 	if(xhr.readyState === 4){
 		var employees = JSON.parse(xhr.responseText);
-		var statusHTML = '<ul class="bulleted">';
+		var statusHTML = '<p>Employee status list</p><ul class="bulleted">';
 		for (var i = 0; i < employees.length; i += 1) {
 			statusHTML += 
 			(employees[i].inoffice ? '<li><span class="glyphicon glyphicon-ok in"></span>' : 
 			'<li><span class="glyphicon glyphicon-remove out"></span>') + employees[i].name + '</li>';
 		};
 		document.getElementById('employee-list').innerHTML = statusHTML;
-		//console.log(statusHTML);
 	}
 };
 xhr.open('GET', 'data/employees.json');
 xhr.send();
 
-console.log('things are happening');  
+
+
+var rooms = new XMLHttpRequest();
+rooms.onreadystatechange = function(){
+	if(rooms.readyState === 4){
+		var room = JSON.parse(rooms.responseText);
+		var statusHTML = '<p>Room status list</p><ul class="bulleted">';
+		for (var i = 0; i < room.length; i += 1) {
+			statusHTML += 
+			(room[i].inoffice ? '<li><span class="glyphicon glyphicon-ok in"></span>' : 
+			'<li><span class="glyphicon glyphicon-remove out"></span>') + room[i].name + ' ' + '</li>';
+		};
+		document.getElementById('room-list').innerHTML = statusHTML;
+	}
+};
+rooms.open('GET', 'data/rooms.json');
+rooms.send();
+
